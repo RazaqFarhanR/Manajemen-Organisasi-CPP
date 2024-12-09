f24ea03a0d1595776bf57776ecee551a0b6e8d52
RazaqFarhanR <aljawadfarhan@gmail.com>
2024-12-09 23:09:46 +0700

    feat: add departement ADT

--- orgManager.h
+++ orgManager.h
@@ -2,34 +2,58 @@
 #define ORGMANAGER_H_INCLUDED
 
 #include <iostream>
-#define first(L) L.first
-#define next(p) p->next
 #define info(p) p->info
+#define next(p) p->next
+#define first(L) ((L).first)
+#define last(L) ((L).last)
 
 using namespace std;
 
 struct organisasi {
-    string id;
     string nama;
+    string kontak;
     int tahunBerdiri;
 };
 
 typedef struct elemenOrg *adr_org;
-typedef organisasi infotype;
+typedef organisasi infotypeOrg;
+
+struct departemen {
+    string nama;
+    string koor;
+    int jumlahAnggota;
+};
+
+typedef struct elemenDep *adr_dep;
+typedef departemen infotypeDep;
 
 struct elemenOrg {
-    infotype info;
+    adr_org prev;
+    infotypeOrg info;
+    adr_dep departemen;
     adr_org next;
+};
+
+struct elemenDep {
+    infotypeDep info;
+    adr_dep next;
 };
 
 struct ListOrg {
     adr_org first;
+    adr_org last;
 };
+
+struct ListDep {
+    adr_dep first;
+};
+
+void menu();
+bool isEmpty(ListOrg L);
 
 void createListOrg(ListOrg &L);
 adr_org createElmOrg(infotype x);
 void insertLastOrg(ListOrg &L, adr_org P);
-bool isEmpty(ListOrg L);
 void deleteFirstOrg(ListOrg &L, adr_org &P);
 void deleteAfterOrg(ListOrg &L, adr_org Prec, adr_org &P);
 void deleteLastOrg(ListOrg &L, adr_org &P);
